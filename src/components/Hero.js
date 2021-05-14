0ac2d045a8f146efcbd8a4be9528c9d676109689
Tibor Gazsovics <gazsovicst@gmail.com>
2021-05-14 19:52:50 +0100

    finished Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,26 +7,74 @@ const Section = styled.section`
     width: 100%;
     background: url(${img});
     background-position: center;
+    position: relative;
 `;
-const Title = styled.h1``;
-const Description = styled.p``;
-const Button = styled.button``;
+const HeroContainer = styled.div`
+    height: auto;
+    width: 400px;
+    position: absolute;
+    left: 20%;
+    top: 60%;
+    transform: translate(-20%, -60%);
+    background-color: rgba(238,238,238, 0.75);
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 2px 2px 2px #404040;
+
+`;
+const Title = styled.h1`
+    font-weight: 600;
+    font-size: 2rem;
+    margin-bottom: 30px; 
+`;
+const Description = styled.p`
+    font-weight: 400;
+    font-size: 1.2rem;
+    line-height: 25px;
+`;
+const ButtonRow = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+`;
+const Button = styled.button`
+    font-weight: 400;
+    font-size: 1.2rem;
+    margin-top: 30px;
+    padding: 10px 25px;
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(2px);
+    border: 1px solid #383838;
+    cursor: pointer;
+    width: 150px;
+    transition: 300ms ease-in;
+    &:hover {
+        backdrop-filter: blur(5px); 
+        color: #101010;
+        border: 1px solid #101010;
+    }
+`;
 
 
 const Hero = () => {
     return (
         <Section>
-            <Title>
-                Mysterious Nature
-                <Description>
-                    Explore nature at its finest!<br />
-                    Most importantly, enjoy the peace and quiet.
-                    Soon, you will be the same! HA HA HAAA! MUHA HAHAAAA!
-                    <Button>
-                        Explore
-                    </Button>
-                </Description>
-            </Title>
+            <HeroContainer>
+                <Title>
+                    Mysterious Nature
+                </Title>
+                    <Description>
+                        Explore nature at its finest!<br />
+                        Spend a weekend with your family and relax.
+                    </Description>
+
+                    <ButtonRow>
+                        <Button>
+                            Explore
+                        </Button>   
+                    </ButtonRow>            
+            </HeroContainer>
         </Section>
     )
 }
--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -1,11 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
     * {
-        color: green;
+        /* color: green; */
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif;
+        color: #383838;
     }
 `
 
